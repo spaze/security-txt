b81c8ffbcad22ce5375d8d6cb2757be11188dba3
Michal Špaček <mail@michalspacek.cz>
2023-08-03 00:45:15 +0200

    URLs can also be without a schema and they're defo not phone numbers

--- src/Violations/SecurityTxtContactNotUri.php
+++ src/Violations/SecurityTxtContactNotUri.php
@@ -10,15 +10,17 @@ class SecurityTxtContactNotUri extends SecurityTxtFieldNotUri
 
 	public function __construct(string $uri)
 	{
-		$isEmail = (bool)filter_var($uri, FILTER_VALIDATE_EMAIL);
-		parent::__construct(
-			SecurityTxtField::Contact,
-			$uri,
-			'draft-foudil-securitytxt-03',
-			($isEmail ? 'mailto:' : 'tel:') . $uri,
-			$isEmail ? 'The value looks like an email address, add the "mailto" schema' : 'The value looks like a phone number, add the "tel" schema',
-			'2.5.3',
-		);
+		if (filter_var($uri, FILTER_VALIDATE_EMAIL)) {
+			$correctValue = "mailto:{$uri}";
+			$howToFix = 'The value looks like an email address, add the "mailto" schema';
+		} elseif (preg_match('/^\+?[0-9\-. ]+$/', $uri)) {
+			$correctValue = "tel:{$uri}";
+			$howToFix = 'The value looks like a phone number, add the "tel" schema';
+		} else {
+			$correctValue = "https://{$uri}";
+			$howToFix = 'The value looks like a hostname, add the "https" schema';
+		}
+		parent::__construct(SecurityTxtField::Contact, $uri, 'draft-foudil-securitytxt-03', $correctValue, $howToFix, '2.5.3');
 	}
 
 }
